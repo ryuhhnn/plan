01c1802bd415ed699ea68287916c6f72793d1196
Ryan Coleman <me@ryuhhnn.com>
2016-10-14 13:42:01 -0500

    Adds some details to EventDetail

--- js/pages/EventDetail.js
+++ js/pages/EventDetail.js
@@ -27,19 +27,22 @@ export default class EventDetail extends ParseComponent {
       height: "100%"
     };
 
+    var eventName = this.data.events.map(function(e) {
+      return e.name;
+    });
+
+    var eventDetails = this.data.events.map(function(e) {
+      return e.details;
+    });
+
     return (
       <div class="container" style={marginStyle}>
         <Grid>
           <Row className="show-grid">
             <Col md={6}>
-              <h1>Event Name<small>Event Date</small></h1>
-              <address>
-                <strong>Location Name</strong><br />
-                1234 Some Street<br />
-                St. Louis, MO 63123<br />
-              </address>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vulputate nisl id augue aliquam, non consequat nisl dignissim. Aliquam erat volutpat. Nullam sed leo a augue consequat venenatis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce et tortor ut arcu iaculis pellentesque. Suspendisse eget ex at nunc tempor volutpat non ut odio. Aenean dictum purus nec neque tincidunt, ac venenatis tellus consectetur. Donec tortor sem, tincidunt et nibh id, mattis blandit felis. Integer condimentum non quam vitae venenatis. Morbi quis tortor nunc. In tempor turpis mi, varius vulputate dolor luctus sit amet. Morbi feugiat vulputate mi ut imperdiet. Nam condimentum augue ut augue aliquam, sit amet eleifend diam suscipit. Aenean tristique lorem diam, pulvinar blandit neque consectetur id. Sed nec auctor enim.</p>
-              <p></p>
+              <h1>{eventName}<small>Event Date</small></h1>
+              <address><strong>Location Name</strong><br />1234 Some Street<br />St. Louis, MO 63123<br /></address>
+              <p>{eventDetails}</p>
               <EditEvent />
             </Col>
             <Col md={6}>
